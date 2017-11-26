7b54c9c34034cfb6aea3383eb0708c4f79e91e48
Stalf <kssgel@yandex.ru>
2017-11-26 16:45:37 +0500

    minorfixes

--- site/web/src/main/java/com/droidwars/web/controller/UserController.java
+++ site/web/src/main/java/com/droidwars/web/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.droidwars.web.controller;
 
-import com.droidwars.entity.User;
-import com.droidwars.service.UserService;
+import com.droidwars.core.entity.User;
+import com.droidwars.core.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
